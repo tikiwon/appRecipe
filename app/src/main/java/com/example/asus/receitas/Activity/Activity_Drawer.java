bf44855c44f2cf7ab0ac06a1e207fd28d73e6290
Vitor Giammella <vitor.giammella@soaexpert.com.br>
2017-11-23 16:49:20 -0200

    Created usuario class

--- app/src/main/java/com/example/asus/receitas/Activity/Activity_Drawer.java
+++ app/src/main/java/com/example/asus/receitas/Activity/Activity_Drawer.java
@@ -21,6 +21,7 @@ import com.example.asus.receitas.R;
 public class Activity_Drawer extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,15 +29,16 @@ public class Activity_Drawer extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
+// Menu trez pontinhos
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
---
+++ app/src/main/java/com/example/asus/receitas/Classes/Mural.java
@@ -0,0 +1,14 @@
+package com.example.asus.receitas.Classes;
+
+public class Mural {
+
+    private String texto;
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+}
